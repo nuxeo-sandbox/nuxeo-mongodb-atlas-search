8f8a21bb5a8980aeea9c35f968408cf7d4153e45
Michaël Vachette <5880528+michaelva@users.noreply.github.com>
2024-08-19 15:04:41 +0900

    WIP

--- nuxeo-mongodb-atlas-search-core/src/main/java/org/nuxeo/labs/atlas/search/pp/MongoDBAtlasSearchPP.java
+++ nuxeo-mongodb-atlas-search-core/src/main/java/org/nuxeo/labs/atlas/search/pp/MongoDBAtlasSearchPP.java
@@ -2,27 +2,229 @@ package org.nuxeo.labs.atlas.search.pp;
 
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.search.SearchOperator;
+import com.mongodb.client.model.search.SearchPath;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.bson.BsonDocument;
+import org.bson.BsonValue;
 import org.bson.Document;
-import org.nuxeo.ecm.core.api.DocumentModel;
+import org.bson.conversions.Bson;
+import org.bson.json.JsonMode;
+import org.bson.json.JsonWriterSettings;
+import org.nuxeo.ecm.core.api.*;
+import org.nuxeo.ecm.platform.query.api.Aggregate;
+import org.nuxeo.ecm.platform.query.api.AggregateDefinition;
+import org.nuxeo.ecm.platform.query.api.Bucket;
+import org.nuxeo.ecm.platform.query.core.AggregateBase;
+import org.nuxeo.ecm.platform.query.core.BucketTerm;
 import org.nuxeo.ecm.platform.query.nxql.CoreQueryDocumentPageProvider;
 import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.runtime.mongodb.MongoDBConnectionConfig;
 import org.nuxeo.runtime.mongodb.MongoDBConnectionService;
+import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Projections;
+import com.mongodb.client.model.search.SearchCount;
+import com.mongodb.client.model.search.SearchOptions;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
+import static org.nuxeo.ecm.platform.query.api.PageProviderService.NAMED_PARAMETERS;
 
 public class MongoDBAtlasSearchPP extends CoreQueryDocumentPageProvider {
 
     private static final Logger log = LogManager.getLogger(MongoDBAtlasSearchPP.class);
 
+    protected HashMap<String, Aggregate<? extends Bucket>> currentAggregates;
+
     @Override
     public List<DocumentModel> getCurrentPage() {
+        long t0 = System.currentTimeMillis();
+
+        // use a cache
+        if (currentPageDocuments != null) {
+            return currentPageDocuments;
+        }
+
+        search();
+
+        // send event for statistics !
+        fireSearchEvent(getCoreSession().getPrincipal(), query, currentPageDocuments, System.currentTimeMillis() - t0);
+        return currentPageDocuments;
+    }
+
+    @Override
+    public boolean hasAggregateSupport() {
+        return true;
+    }
+
+    @Override
+    public Map<String, Aggregate<? extends Bucket>> getAggregates() {
+        getCurrentPage();
+        return currentAggregates;
+    }
+
+    public MongoCollection<Document> getCollection(CoreSession session) {
+        String repositoryName = session.getRepositoryName();
         MongoDBConnectionService mongoService = Framework.getService(MongoDBConnectionService.class);
-        MongoDatabase database = mongoService.getDatabase("nuxeo");
-        MongoCollection<Document> collection = database.getCollection("default");
-        return new ArrayList<>();
+        MongoDBConnectionConfig config = mongoService.getConfig(repositoryName);
+        MongoDatabase database = mongoService.getDatabase(config.dbname);
+        return database.getCollection(repositoryName);
+    }
+
+    public List<DocumentModel> extractHits(CoreSession session, Document result) {
+        List<DocumentModel> hits;
+        List<Document> docs = result.getList("docs", Document.class);
+        IdRef[] ids = docs.stream().map(doc -> doc.toBsonDocument().getString("ecm:id").getValue()).map(IdRef::new).toArray(IdRef[]::new);
+        hits = session.getDocuments(ids);
+        return hits;
+    }
+
+    public HashMap<String, Aggregate<? extends Bucket>> extractFacetBuckets(Document result) {
+        HashMap<String, Aggregate<? extends Bucket>> aggregates = new HashMap<>();
+
+        BsonDocument meta = result.toBsonDocument().getDocument("meta");
+        if (meta == null) {
+            return aggregates;
+        }
+        BsonDocument facets = meta.getDocument("facet");
+        if (facets == null) {
+            return aggregates;
+        }
+
+        List<AggregateDefinition> aggregateDefinitions = getAggregateDefinitions();
+
+        for (Map.Entry<String, BsonValue> facet : facets.toBsonDocument().entrySet()) {
+            String name = facet.getKey();
+            BsonValue[] buckets = facet.getValue().asDocument().getArray("buckets").toArray(BsonValue[]::new);
+            List<BucketTerm> parsedBucket = Arrays.stream(buckets).map(
+                            bucket -> new BucketTerm(
+                                    bucket.asDocument().getString("_id").getValue(),
+                                    bucket.asDocument().getInt64("count").getValue())
+                    )
+                    .toList();
+            AggregateDefinition def = aggregateDefinitions.stream().filter(predicate -> name.equals(predicate.getId())).findFirst().orElse(null);
+            AggregateBase<BucketTerm> base = new AggregateBase<BucketTerm>(def, getSearchDocumentModel());
+            base.setBuckets(parsedBucket);
+            aggregates.put(name, base);
+        }
+
+        return aggregates;
+    }
+
+    public boolean runWithFacets() {
+        List<AggregateDefinition> aggregateDefinitions = getAggregateDefinitions();
+        boolean skip = isSkipAggregates();
+        return !(skip || aggregateDefinitions.isEmpty());
+    }
+
+    public Document buildFacets() {
+        Document facets = new Document();
+        for (AggregateDefinition def : getAggregateDefinitions()) {
+            facets.append(def.getId(), new Document("type", "string").append("path", def.getDocumentField()));
+        }
+        return facets;
+    }
+
+    public List<SearchOperator> buildFacetFilter() {
+        List<SearchOperator> filters = new ArrayList<>();
+        for (AggregateDefinition def : getAggregateDefinitions()) {
+            DocumentModel searchDoc = getSearchDocumentModel();
+            String value = (String) searchDoc.getProperty(def.getSearchField().getSchema(),def.getSearchField().getName());
+            if (value != null) {
+                filters.add(
+                        SearchOperator.of(new Document("equals",
+                                new Document("path", def.getDocumentField()).append("value", value))));
+            }
+        }
+        return filters;
+    }
+
+    public void search() {
+        DocumentModel searchDoc = getSearchDocumentModel();
+        Map<String, String> namedParameters = (Map<String, String>) searchDoc.getContextData(NAMED_PARAMETERS);
+
+        CoreSession session = getCoreSession();
+        MongoCollection<Document> collection = getCollection(session);
+
+        List<Bson> stages = new ArrayList<>();
+
+        // $search stage
+        SearchOptions options = SearchOptions.searchOptions()
+                .index("default")
+                .count(SearchCount.total());
+
+        SearchOperator operator;
+
+        if (runWithFacets()) {
+            SearchOperator innerOp = SearchOperator.text(SearchPath.wildcardPath("*"), namedParameters.get("input_text"));
+            List<SearchOperator> facetFilters = buildFacetFilter();
+            if (!facetFilters.isEmpty()) {
+                innerOp = SearchOperator.compound().must(List.of(innerOp)).filter(facetFilters);
+            }
+            operator = SearchOperator.of(
+                    new Document("facet",
+                            new Document("operator", innerOp)
+                                    .append("facets", buildFacets())));
+            System.out.println(format(operator.toBsonDocument()));
+        } else {
+            operator = SearchOperator.text(SearchPath.wildcardPath("*"), namedParameters.get("input_text"));
+        }
+
+        Bson searchStage = Aggregates.search(operator, options);
+        System.out.println(format(searchStage.toBsonDocument()));
+        stages.add(searchStage);
+
+        if (runWithFacets()) {
+            // $facet stage
+            List<Bson> facetStages = new ArrayList<>();
+            facetStages.add(Aggregates.skip((int) getCurrentPageOffset()));
+            facetStages.add(Aggregates.limit((int) getPageSize()));
+
+            //Set document fields to include in the response
+            List<Bson> projections = new ArrayList<>();
+            projections.add(Projections.include("ecm:id"));
+
+            facetStages.add(Aggregates.project(Projections.fields(projections)));
+            Bson facetStage = new Document("$facet",
+                    new Document("docs", facetStages)
+                            .append("meta",
+                                    Arrays.asList(new Document("$replaceWith", "$$SEARCH_META"), Aggregates.limit(1)))
+            );
+
+            System.out.println(format(facetStage.toBsonDocument()));
+            stages.add(facetStage);
+        }
+
+        //set stage
+        Bson setStage = new Document("$set",
+                new Document("meta",
+                        new Document("$arrayElemAt",
+                                Arrays.asList("$meta", 0))));
+
+        System.out.println(format(setStage.toBsonDocument()));
+
+        stages.add(setStage);
+
+        //do search
+        AggregateIterable<Document> aggregationResults = collection.aggregate(stages);
+
+        Document first = aggregationResults.first();
+
+        System.out.println(format(first.toBsonDocument()));
+
+        currentPageDocuments = extractHits(session, first);
+
+        if (runWithFacets()) {
+            currentAggregates = extractFacetBuckets(first);
+        }
+    }
+
+    private static String format(BsonDocument document) {
+        var settings = JsonWriterSettings.builder()
+                .indent(true).outputMode(JsonMode.SHELL).build();
+        return document.toJson(settings);
     }
 
 }
--- nuxeo-mongodb-atlas-search-core/src/test/java/org/nuxeo/labs/atlas/search/TestAtlasPageProvider.java
+++ nuxeo-mongodb-atlas-search-core/src/test/java/org/nuxeo/labs/atlas/search/TestAtlasPageProvider.java
@@ -3,10 +3,14 @@ package org.nuxeo.labs.atlas.search;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.nuxeo.ecm.automation.core.util.PageProviderHelper;
+import org.nuxeo.ecm.core.api.CoreInstance;
 import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.test.DefaultRepositoryInit;
 import org.nuxeo.ecm.core.test.annotations.Granularity;
 import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
+import org.nuxeo.ecm.platform.query.api.Aggregate;
+import org.nuxeo.ecm.platform.query.api.Bucket;
 import org.nuxeo.ecm.platform.query.api.PageProvider;
 import org.nuxeo.ecm.platform.query.api.PageProviderDefinition;
 import org.nuxeo.ecm.platform.test.PlatformFeature;
@@ -14,27 +18,32 @@ import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
 
-import javax.inject.Inject;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RunWith(FeaturesRunner.class)
 @Features({PlatformFeature.class})
 @RepositoryConfig(init = DefaultRepositoryInit.class, cleanup = Granularity.METHOD)
 @Deploy({
         "nuxeo-mongodb-atlas-search-core",
+        "org.nuxeo.ecm.core.storage.mongodb",
         "nuxeo-mongodb-atlas-search-core:test-mongodb-connection-config.xml"
 })
 public class TestAtlasPageProvider {
 
-    @Inject
-    protected CoreSession session;
-
     @Test
     public void testPP() {
+        CoreSession session = CoreInstance.getCoreSession("default");
         PageProviderDefinition def = PageProviderHelper.getPageProviderDefinition("simple-atlas-search");
         HashMap<String,String> namedParameters = new HashMap<>();
-        PageProvider<?> pp = PageProviderHelper.getPageProvider(session, def, namedParameters);
-        pp.getCurrentPage();
+        namedParameters.put("input_text","apple");
+        namedParameters.put("ecm_primarytype_agg","File");
+        PageProvider<DocumentModel> pp = (PageProvider<DocumentModel>) PageProviderHelper.getPageProvider(session, def, namedParameters);
+        List<DocumentModel> results = pp.getCurrentPage();
+        System.out.println(results);
+        Map<String, Aggregate<? extends Bucket>> aggregates = pp.getAggregates();
+        System.out.println(aggregates);
     }
 
 }
