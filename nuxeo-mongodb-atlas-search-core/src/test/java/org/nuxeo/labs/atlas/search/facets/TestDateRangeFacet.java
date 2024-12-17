6aaed3bb8e80ab32dfaa6173631927725982bcbf
Michaël Vachette <5880528+michaelva@users.noreply.github.com>
2024-12-17 18:35:01 +0900

    Upgrade to Java 21 API

--- nuxeo-mongodb-atlas-search-core/src/test/java/org/nuxeo/labs/atlas/search/facets/TestDateRangeFacet.java
+++ nuxeo-mongodb-atlas-search-core/src/test/java/org/nuxeo/labs/atlas/search/facets/TestDateRangeFacet.java
@@ -22,7 +22,7 @@ import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
 
-import javax.inject.Inject;
+import jakarta.inject.Inject;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
